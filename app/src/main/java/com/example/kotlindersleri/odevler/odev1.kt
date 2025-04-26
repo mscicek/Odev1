3909f5651ce1f5c0a958b640dc035c8d0489b879
mscicek <m.sukrucicek@hotmail.com>
2025-04-26 23:22:58 +0300

    firstcommit

---
+++ app/src/main/java/com/example/kotlindersleri/odevler/odev1.kt
@@ -0,0 +1,59 @@
+package com.example.kotlindersleri.odevler
+
+
+fun main() {
+
+    var sehir = "Konya"
+    var ulke = "Türkiye"
+    var telefon = "155165"
+    var postaKodu = "42040"
+    var email = "m.sukrucicek@hotmail.com"
+    var meslek = "Bilgisayar Mühendisi"
+    var stokMiktari = "515"
+    var musteriAdi = "Kasım Adalan"
+    var bakiye = "3500000000"
+    var dogumGunu = "10.10.2002"
+    var maas = "150000"
+    var medeniDurum = "Bekar"
+    var urunYorum = "Mükemmel"
+    var odemeTarihi = "27.04u.2025"
+    var odeme = "Yapıldı"
+    var siparisAdeti = "42"
+    var arabaModeli = "Mercedes"
+    var kitapAdi = "İnsan Ne ile Yaşar?"
+    var yayinlamaTarihi = "01.05.2025"
+    var indirimMiktari = "%50"
+    var odaSayisi = "8"
+    var enlem = "36-42"
+    var boylam = "26-45"
+    var urunAdi = "Çikolata"
+    var yemekFiyati = "1800"
+    var marka = "Philips"
+    var muzikAdi = "El Gibi"
+    var videoSuresi = "28 dk"
+    var urunPuani = "5.0"
+    var resimAdi = "Mona Lisa"
+    var dosyaFormati = "pdf"
+    var renk = "Yeşil"
+    var renkKodu = "424152"
+    var telefonModeli = "Iphone 16"
+    var ekranBoyutu = "16.1"
+    var agirlik = "1.5"
+    var ulusalGun = "23 Nisan"
+    var tatilGunu = "23 Nisan"
+    var rezervasyonTarihi = "15.05.2025"
+    var sokakAdi = "Yıldız"
+    var otobusHatti = "14"
+    var kalanDakika = "3 dk"
+    var takipKodu = "SDH14528"
+    var kuponSuresi = "15 gün"
+    var kuponKodu = "MŞÇ1905"
+    var faturaAdresi = ".... caddesi ...."
+
+    println(renkKodu)
+
+    println(kitapAdi)
+
+
+}
+
---
+++ app/src/main/java/com/example/kotlindersleri/standart_programlama/Döngüler.kt
@@ -0,0 +1,13 @@
+package com.example.kotlindersleri.standart_programlama
+
+fun main() {
+
+    ///1,2,3
+    for (i in 1..3)
+        println("Döngü 1: $i")
+
+    for (x in 20 downTo 10 step 5)
+        println("Döngü 3 : $x")
+
+
+}
